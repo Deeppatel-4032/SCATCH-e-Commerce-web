a7f6352b29334aece6cd2986040701e2a6aa6f98
Deeppatel-4032 <deeppatel7868@gmail.com>
2025-04-01 11:40:40 +0530

    owner check isloggedin

--- routes/owener_route.js
+++ routes/owener_route.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const owner_router = express.Router();
 const ownerCon = require("../controllers/owners_Controller.js")
+const isLoggedIn = require("../middlewares/isLoggedIn.js");
 
 owner_router.get("/ownerCreateForm", ownerCon.getOwnerData);
-owner_router.post("/ownerCreate", ownerCon.postOwnerData);
+owner_router.post("/ownerCreate", isLoggedIn, ownerCon.postOwnerData);
 
 owner_router.get("/adminLogInForm", ownerCon.getAdminData);
-owner_router.post("/adminLogin", ownerCon.postAdminData)
+owner_router.post("/adminLogin", isLoggedIn, ownerCon.postAdminData);
 
 
 module.exports = owner_router;
